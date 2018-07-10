aa1c017e9d0763a19531d1cca17714497e63329e
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-10 14:47:50 +0700

    Fix loop

--- assertable/assertable.go
+++ assertable/assertable.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/chonla/yas/response"
 	"github.com/fatih/color"
+	"github.com/kr/pretty"
 )
 
 // Assertable is something assertable
@@ -43,9 +44,15 @@ func (a *Assertable) Assert(ex map[string]string) error {
 	green := color.New(color.FgGreen).SprintFunc()
 	blue := color.New(color.FgBlue).SprintFunc()
 
+	if len(ex) == 0 {
+		return errors.New("no assertion given")
+	}
+
+	pretty.Println(ex)
+
 	fmt.Printf("%s\n", magenta("----"))
 	for k, v := range ex {
-		fmt.Printf("Test %s with %s...", blue(k), blue(v))
+		fmt.Printf("Assert %s with %s...", blue(k), blue(v))
 		k = strings.ToLower(k)
 		if val, ok := a.values[k]; ok {
 			match := false
@@ -57,15 +64,15 @@ func (a *Assertable) Assert(ex map[string]string) error {
 			}
 			if match {
 				fmt.Printf("%s\n", green("PASS"))
-				return nil
+			} else {
+				fmt.Printf("%s\n", red("FAILED"))
+				return fmt.Errorf("expect %s in %s, but not", red(v), red(k))
 			}
-			fmt.Printf("%s\n", red("FAILED"))
-			return fmt.Errorf("expect %s in %s, but not", red(v), red(k))
 		} else {
 			fmt.Printf("%s\n", red("FAILED"))
 			return fmt.Errorf("response does not contain %s", k)
 		}
 	}
 
-	return errors.New("no assertion given")
+	return nil
 }
