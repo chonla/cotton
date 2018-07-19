6aa6753ebe436d413ae7d6fc0b5ae4866bd57d18
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-19 21:40:01 +0700

    Add test

--- markdown/markdown_test.go
+++ markdown/markdown_test.go
@@ -1,6 +1,7 @@
 package markdown
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -36,6 +37,39 @@ func TestParseWellFormedMarkdownFile(t *testing.T) {
 
 	assert.Nil(t, result)
 	assert.Equal(t, 2, md.Len())
+}
+
+func TestParseWindowsMarkdownFile(t *testing.T) {
+	readFileFn = func(filename string) ([]byte, error) {
+		markdown := "# Title\r\n\r\n## POST /login"
+
+		return []byte(markdown), nil
+	}
+
+	md := NewMD()
+
+	result := md.Parse("file.md")
+
+	assert.Nil(t, result)
+	assert.Equal(t, 2, md.Len())
+}
+
+func TestParseErrorMarkdownFile(t *testing.T) {
+	readFileFn = func(filename string) ([]byte, error) {
+		markdown := `
+			# Title
+
+			## POST /login
+		`
+
+		return []byte(markdown), errors.New("oh!")
+	}
+
+	md := NewMD()
+
+	result := md.Parse("file.md")
+
+	assert.NotNil(t, result)
 }
 
 func TestCursorShouldReturnTrueIfItemAvailable(t *testing.T) {
@@ -110,4 +144,22 @@ func TestGetValueAtCursor(t *testing.T) {
 		},
 		Text: "PUT /login",
 	})
+}
+
+func TestGetValueAtTheEnd(t *testing.T) {
+	markdown := `
+		# Title
+
+		## PUT /login
+	`
+
+	md := NewMD()
+
+	md.ParseString(markdown)
+
+	md.Next()
+	md.Next()
+	md.Next()
+
+	assert.Nil(t, md.Value())
 }
