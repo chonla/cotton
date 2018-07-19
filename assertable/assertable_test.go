1041f1a34eaed99dd15c0cb271cfbfd9466bd020
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-20 00:11:30 +0700

    Add test

--- assertable/assertable_test.go
+++ assertable/assertable_test.go
@@ -55,3 +55,78 @@ func TestAssertWithNoAssertion(t *testing.T) {
 
 	assert.Equal(t, errors.New("no assertion given"), result)
 }
+
+func TestAssertionAllPass(t *testing.T) {
+	jsonString := "{ \"data\": \"ok\", \"list\": [0, 1, 2] }"
+
+	response := &response.Response{
+		Proto:      "http",
+		Status:     "200 OK",
+		StatusCode: 200,
+		Header: map[string][]string{
+			"content-type": []string{
+				"application/json; charset=utf-8",
+			},
+		},
+		Body: jsonString,
+	}
+
+	assertable := NewAssertable(response)
+
+	result := assertable.Assert(map[string]string{
+		"header.content-type": "application/json; charset=utf-8",
+		"data.list[1]":        "1",
+	})
+
+	assert.Nil(t, result)
+}
+
+func TestAssertionSomeFailShouldGiveResultFail(t *testing.T) {
+	jsonString := "{ \"data\": \"ok\", \"list\": [0, 1, 2] }"
+
+	response := &response.Response{
+		Proto:      "http",
+		Status:     "200 OK",
+		StatusCode: 200,
+		Header: map[string][]string{
+			"content-type": []string{
+				"application/json; charset=utf-8",
+			},
+		},
+		Body: jsonString,
+	}
+
+	assertable := NewAssertable(response)
+
+	result := assertable.Assert(map[string]string{
+		"header.content-type": "application/json; charset=utf-8",
+		"data.list[1]":        "0",
+	})
+
+	assert.NotNil(t, result)
+}
+
+func TestAssertionNonExistingShouldGiveResultFail(t *testing.T) {
+	jsonString := "{ \"data\": \"ok\", \"list\": [0, 1, 2] }"
+
+	response := &response.Response{
+		Proto:      "http",
+		Status:     "200 OK",
+		StatusCode: 200,
+		Header: map[string][]string{
+			"content-type": []string{
+				"application/json; charset=utf-8",
+			},
+		},
+		Body: jsonString,
+	}
+
+	assertable := NewAssertable(response)
+
+	result := assertable.Assert(map[string]string{
+		"header.content-type": "application/json; charset=utf-8",
+		"data.not-here":       "0",
+	})
+
+	assert.NotNil(t, result)
+}
