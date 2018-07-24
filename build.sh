e677017afbb39e3bb011a420f1f1a7cfcb44abcd
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-07-24 10:55:40 +0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux
+
+export GOPATH="${pwd}/.gobuild"
+SRCDIR="${GOPATH}/src/github.com/chonla/cotton"
+
+[ -d ${GOPATH} ] && rm -rf ${GOPATH}
+mkdir -p ${GOPATH}/{src,pkg,bin}
+mkdir -p ${SRCDIR}
+cp tf.go ${SRCDIR}
+(
+    echo ${GOPATH}
+    cd ${SRCDIR}
+    go get .
+    go install .
+)
