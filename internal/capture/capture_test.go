dfe2b08e7a57c75a4868ec97a54f50e0d66ac000
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-06-30 21:39:38 +0700

    chore: Add tests

--- internal/capture/capture_test.go
+++ internal/capture/capture_test.go
@@ -43,3 +43,68 @@ func TestCaptureFromPlainAssignmentWithWhiteSpaces(t *testing.T) {
 		Selector: "$.data.firstname",
 	}, result)
 }
+
+func TestCaptureFromNonCapture(t *testing.T) {
+	mdLine := line.Line("-   name :  `$.data.firstname`")
+
+	result, ok := capture.Try(mdLine)
+
+	assert.False(t, ok)
+	assert.Nil(t, result)
+}
+
+func TestCapturesAreSimilarIfTheyHaveSameNameAndSelector(t *testing.T) {
+	cap1 := capture.New("cool.name", "cool.value")
+	cap2 := capture.New("cool.name", "cool.value")
+
+	result1 := cap1.SimilarTo(cap2)
+	result2 := cap2.SimilarTo(cap1)
+
+	assert.True(t, result1)
+	assert.True(t, result2)
+}
+
+func TestCapturesAreSimilarIfTheyHaveDifferentNames(t *testing.T) {
+	cap1 := capture.New("cool.Name", "cool.value")
+	cap2 := capture.New("cool.name", "cool.value")
+
+	result1 := cap1.SimilarTo(cap2)
+	result2 := cap2.SimilarTo(cap1)
+
+	assert.False(t, result1)
+	assert.False(t, result2)
+}
+
+func TestCapturesAreSimilarIfTheyHaveDifferentValues(t *testing.T) {
+	cap1 := capture.New("cool.name", "cool.Value")
+	cap2 := capture.New("cool.name", "cool.value")
+
+	result1 := cap1.SimilarTo(cap2)
+	result2 := cap2.SimilarTo(cap1)
+
+	assert.False(t, result1)
+	assert.False(t, result2)
+}
+
+func TestCloningMustReturnSameValue(t *testing.T) {
+	cap1 := capture.New("cool.name", "cool.Value")
+	cap2 := cap1.Clone()
+
+	result1 := cap1.SimilarTo(cap2)
+	result2 := cap2.SimilarTo(cap1)
+
+	assert.True(t, result1)
+	assert.True(t, result2)
+}
+
+func TestCloningMustReturnACopyOfTheSource(t *testing.T) {
+	cap1 := capture.New("cool.name", "cool.Value")
+	cap2 := cap1.Clone()
+	cap2.Name = "cooler.name"
+
+	result1 := cap1.SimilarTo(cap2)
+	result2 := cap2.SimilarTo(cap1)
+
+	assert.False(t, result1)
+	assert.False(t, result2)
+}
